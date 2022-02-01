983211d8ad4a977522ae2e5f94c1a3263698fc2d
Frappuccino001 <fumi00hiro@icloud.com>
2022-02-01 22:10:16 +0900

    数字1つを取得

--- main.go
+++ main.go
@@ -4,18 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-// import "strconv"
 // import "strings"
 
 func getSingleString() {
 	// 文字列１つのみ
+	// 例
+	// ==========
+	// sample
+	// ==========
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	s := sc.Text()
 	fmt.Println(s)
 }
 
+func getSingleInt() {
+	// 文字列１つのみ
+	// 例
+	// ==========
+	// sample
+	// ==========
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	fmt.Println(i)
+}
+
 func main() {
+	// getSingleString()
+	getSingleInt()
 }
